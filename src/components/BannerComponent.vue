ed5acb71a226f42e3be1404ecb70c8bcf6b3a564
Amina N Oti <Amina.Oti@ibm.com>
2022-06-19 19:34:37 +0100

    fix: banner image not displaying

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -2,18 +2,20 @@
   <div>
     <h1>{{ this.websiteTitle }}</h1>
     <h3>{{ this.websiteDescription }}</h3>
-    <img src="@/assets/images/websiteImage.jpeg" alt="website-image" />
+    <img v-bind:src="this.websiteImage" alt="website-image" />
   </div>
 </template>
 
 <script>
+import image from "../assets/images/websiteImage.jpeg";
+
 export default {
   name: "BannerComponent",
   data() {
     return {
       websiteTitle: process.env.VUE_APP_WEBSITE_TITLE,
       websiteDescription: process.env.VUE_APP_WEBSITE_DESCRIPTION,
-      websiteImage: "@/assets/images/websiteImage.jpeg",
+      websiteImage: image,
     };
   },
 };
