cf89183830621c4c905b128c349be24e30e110a4
Amina N Oti <Amina.Oti@ibm.com>
2022-07-26 09:38:22 +0100

    fix: jest test which where broken by vueper

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/unit/fileTransformer.js',
+      '<rootDir>/tests/testConfig/fileTransformer.js',
+  },
+  moduleNameMapper: {
+    '^.+\\.(css|less)$': '<rootDir>/tests/testConfig/cssStub.js',
   },
   "collectCoverage": true,
   "collectCoverageFrom": ["**/src/**/*.{js,vue}", "!**/node_modules/**"],
